dbcafe7fe93fc8bed53391decaad42afdbba4424
arunkapila <arunj.kapila@gmail.com>
2023-12-22 22:32:48 +0400

    Updated paperweight version

--- src/main/java/com/starshootercity/origins/Phantom.java
+++ src/main/java/com/starshootercity/origins/Phantom.java
@@ -17,6 +17,7 @@ import org.bukkit.potion.PotionEffectType;
 
 @SuppressWarnings("unused")
 public class Phantom implements Listener {
+
     @EventHandler
     public void onServerTickEnd(ServerTickEndEvent ignored) {
         for (Player player : Bukkit.getOnlinePlayers()) {
