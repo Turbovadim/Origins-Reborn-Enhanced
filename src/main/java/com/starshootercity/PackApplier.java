f26e63f9d7ccfcf6f2296becce59cff6c60c3a17
arunkapila <arunj.kapila@gmail.com>
2024-01-16 16:53:43 +0000

    Added pack

--- src/main/java/com/starshootercity/PackApplier.java
+++ src/main/java/com/starshootercity/PackApplier.java
@@ -1,6 +1,8 @@
 package com.starshootercity;
 
 import net.kyori.adventure.resource.ResourcePackInfo;
+import net.kyori.adventure.text.Component;
+import org.bukkit.Bukkit;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerJoinEvent;
@@ -13,9 +15,10 @@ public class PackApplier implements Listener {
     public void onPlayerJoin(PlayerJoinEvent event) {
         try {
             if (OriginsReborn.getInstance().getConfig().getBoolean("resource-pack.enabled")) {
-                event.getPlayer().sendResourcePacks(ResourcePackInfo.resourcePackInfo()
-                        .uri(URI.create(OriginsReborn.getInstance().getConfig().getString("resource-pack.link", "https://github.com/cometcake575/Origins-Reborn/blob/main/src/main/Origins%20Pack.zip")))
-                        .computeHashAndBuild().get());
+                ResourcePackInfo packInfo = ResourcePackInfo.resourcePackInfo()
+                        .uri(URI.create(OriginsReborn.getInstance().getConfig().getString("resource-pack.link", "https://minhaskamal.github.io/DownGit/#/home?url=https://github.com/cometcake575/Origins-Reborn/blob/main/src/main/Origins%20Pack.zip")))
+                        .computeHashAndBuild().get();
+                event.getPlayer().sendResourcePacks(packInfo);
             }
         } catch (ExecutionException | InterruptedException e) {
             throw new RuntimeException(e);
