c3aef5b75396121d55a0a9531bcb68c5481a7a65
arunkapila <arunj.kapila@gmail.com>
2024-01-16 16:59:29 +0000

    Modified resource pack link in config.yml to work properly

--- src/main/java/com/starshootercity/PackApplier.java
+++ src/main/java/com/starshootercity/PackApplier.java
@@ -1,8 +1,6 @@
 package com.starshootercity;
 
 import net.kyori.adventure.resource.ResourcePackInfo;
-import net.kyori.adventure.text.Component;
-import org.bukkit.Bukkit;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerJoinEvent;
@@ -16,7 +14,7 @@ public class PackApplier implements Listener {
         try {
             if (OriginsReborn.getInstance().getConfig().getBoolean("resource-pack.enabled")) {
                 ResourcePackInfo packInfo = ResourcePackInfo.resourcePackInfo()
-                        .uri(URI.create(OriginsReborn.getInstance().getConfig().getString("resource-pack.link", "https://minhaskamal.github.io/DownGit/#/home?url=https://github.com/cometcake575/Origins-Reborn/blob/main/src/main/Origins%20Pack.zip")))
+                        .uri(URI.create(OriginsReborn.getInstance().getConfig().getString("resource-pack.link", "https://github.com/cometcake575/Origins-Reborn/raw/main/src/main/Origins%20Pack.zip")))
                         .computeHashAndBuild().get();
                 event.getPlayer().sendResourcePacks(packInfo);
             }
